d4e3eef30b5bd9c6da52da2932b96de3ebe756c5
Josh <jdoleary91@gmail.com>
2021-08-20 19:43:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game'
+
+const coins = [
+  'https://tinyurl.com/react-coin-heads-jpg',
+  'https://www.coinnews.net/wp-content/uploads/2015/03/2015-W-Proof-Roosevelt-Silver-Dime-Reverse.jpg',
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title'>Let's Flip A Coin!</h1>
+      <Game coins={coins} />
     </div>
   );
 }
